88386265f3707d582835cdcc7c8bea98573d6d62
Shin Yi <contact.shinyi@gmail.com>
2021-12-21 22:55:41 +0800

    added redirect to home from error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [])
+
     return (
         <div>
             <h1>Wrong door!</h1>
