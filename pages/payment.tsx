d451eb713acf4e62e2c786939b1fac5c2d88458a
thryec <thryec@gmail.com>
2021-12-23 15:49:15 +0800

    added link back to homepage in result page

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Payment: NextPage = () => {
-  return <div>Payment Result</div>
+  return (
+    <div className="flex justify-center">
+      <div className="bg-slate-200 m-10 p-10 w-1/2 border rounded-md shadow border-gray-300 flex justify-center">
+        Payment Result
+        <Link href="/">
+          <button className="bg-indigo-600 hover:bg-indigo-700 text-white border rounded-md p-2 m-2">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Payment
