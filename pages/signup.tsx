7d1433dfc71d7ce25bdd9df067239a9ca044e954
saetan <saetan@live.com>
2021-12-25 00:02:07 +0800

    set error message for sign up form

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useRouter } from 'next/router';
 
 //To validate Email
@@ -21,6 +21,17 @@ const Signup: React.FC = () => {
 
     const [newAccount, setNewAccount] = useState<LoginDetails>({ email: "", password: "", username: "", walletAddress: "", type: "user" });
     const [isEmailValid, setIsEmailValid] = useState<boolean | null>(null);
+    const [isEmailEmpty, setEmailEmpty] = useState<boolean | null>(null);
+    const [isUserNameEmpty, setUserNameEmpty] = useState<boolean | null>(null);
+    const [isPasswordEmpty, setPasswordEmpty] = useState<boolean | null>(null);
+    const [isWalletAddressEmpty, setWalletAddressEmpty] = useState<boolean | null>(null);
+    const [isAllValid, setAllValid] = useState<boolean>(false);
+
+    const emailInputRef = useRef<HTMLInputElement | undefined | null | any>();
+    const passwordInputRef = useRef<HTMLInputElement | undefined | null | any>();
+    const usernameInputRef = useRef<HTMLInputElement | undefined | null | any>();
+    const walletAddressInputRef = useRef<HTMLInputElement | undefined | null | any>();
+
 
     const handleEmailChange = (event: React.FormEvent<HTMLInputElement>) => {
         setNewAccount({
@@ -29,9 +40,53 @@ const Signup: React.FC = () => {
         });
     }
 
+    const allValidCheck = (): void => {
+        if (emailInputRef && passwordInputRef && usernameInputRef && walletAddressInputRef) {
+            setAllValid(true);
+        } else {
+            setAllValid(false);
+        }
+    }
+
     const onEmailBlur = (): void => {
-        const isValid = validateEmail(newAccount.email);
-        setIsEmailValid(isValid);
+        if (!emailInputRef.current.value) {
+            setEmailEmpty(true);
+        } else {
+            const isValid = validateEmail(newAccount.email);
+            setIsEmailValid(isValid);
+            setEmailEmpty(false);
+        }
+
+        allValidCheck();
+    }
+
+    const onUserNameBlur = (): void => {
+        if (!usernameInputRef.current.value) {
+            setUserNameEmpty(true);
+        } else {
+            setUserNameEmpty(false);
+        }
+
+        allValidCheck();
+    }
+
+    const onPasswordBlur = (): void => {
+        if (!passwordInputRef.current.value) {
+            setPasswordEmpty(true);
+        } else {
+            setUserNameEmpty(false);
+        }
+
+        allValidCheck();
+    }
+    const onWalletAddressBlur = (): void => {
+        if (!walletAddressInputRef.current.value) {
+            setWalletAddressEmpty(true);
+        } else {
+            setUserNameEmpty(false);
+        }
+
+        allValidCheck();
     }
 
     const handlePasswordChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -56,8 +111,8 @@ const Signup: React.FC = () => {
     }
 
     const handleSubmit = async () => {
-
-        //Login Post request
+        //Create user Post request
+        console.log(newAccount);
         const response = await fetch('http://localhost:3001/users', {
             method: 'POST',
             headers: {
@@ -65,7 +120,6 @@ const Signup: React.FC = () => {
             },
             body: JSON.stringify(newAccount),
         });
-
 
         //redirect user to home page
         router.push('/login');
@@ -77,18 +131,24 @@ const Signup: React.FC = () => {
         <div className="flex flex-col text-center">
             <div className="mb-6">
                 <label htmlFor="username" className="customLabel">Please key in a user name:</label>
-                <input id="username" className="customInput" value={newAccount.username} type="text" onChange={handleUserNameChange} />
+                <input id="username" className="customInput" ref={usernameInputRef} value={newAccount.username} type="text" onChange={handleUserNameChange} onBlur={onUserNameBlur} />
+                {isUserNameEmpty ? <span>Please Fill in UserName</span> : ""}
             </div>
             <label htmlFor="email" className="customLabel">Please key in a email:</label>
-            <input id="email" className="customInput" value={newAccount.email} onChange={handleEmailChange} onBlur={onEmailBlur} />
+            <input id="email" className="customInput" ref={emailInputRef} value={newAccount.email} onChange={handleEmailChange} onBlur={onEmailBlur} />
             {isEmailValid == false ? <span>Invalid Email Format</span> : ""}
+            {isEmailEmpty ? <span>Please Fill in Email</span> : ""}
             <label className="customLabel" >Please key in a password:</label>
-            <input className="customInput" value={newAccount.password} type="password" onChange={handlePasswordChange} />
+            <input className="customInput" ref={passwordInputRef} value={newAccount.password} type="password" onChange={handlePasswordChange} onBlur={onPasswordBlur} />
+            {isPasswordEmpty ? <span>Please Fill in Password</span> : ""}
             <label className="customLabel">Please key in your wallet address</label>
-            <input className="customInput" value={newAccount.walletAddress} type="text" onChange={handleWalletAddressChange} />
+            <input className="customInput" ref={walletAddressInputRef} value={newAccount.walletAddress} type="text" onChange={handleWalletAddressChange} onBlur={onWalletAddressBlur} />
+            {isWalletAddressEmpty ? <span>Please Fill in Wallet Address</span> : ""}
         </div>
         <button
+            disabled={!isAllValid}
             type="submit"
+            onClick={handleSubmit}
             className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Confirm
         </button>
