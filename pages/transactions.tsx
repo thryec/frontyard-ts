4a99937649f2510146af43ec7a3aa6379020b4ea
thryec <thryec@gmail.com>
2021-12-29 15:39:15 +0800

    added functions to fetch historical sales and purchase transactions

--- pages/transactions.tsx
+++ pages/transactions.tsx
@@ -1,6 +1,40 @@
 import type { NextPage } from 'next'
 
+const userAddress = '0xe82d5C6B394D9C4dE32F0913e6cE82Dd8dc39226'
+
 const Transactions: NextPage = () => {
+  const fetchPurchases = async () => {
+    console.log('purchases button')
+    try {
+      const res = await fetch(`http://localhost:4000/transactions/purchases?user=${userAddress}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await res.json()
+      console.log('sent database txn: ', data)
+    } catch (err) {
+      console.log('error fetching transactions: ', err)
+    }
+  }
+
+  const fetchSales = async () => {
+    console.log('sales button')
+    try {
+      const res = await fetch(`http://localhost:4000/transactions/sales?user=${userAddress}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await res.json()
+      console.log('sent database txn: ', data)
+    } catch (err) {
+      console.log('error fetching transactions: ', err)
+    }
+  }
+
   return (
     <div>
       <div className="flex justify-center m-5">
@@ -11,10 +45,20 @@ const Transactions: NextPage = () => {
           <h1 className="text-xl underline underline-offset-8 decoration-dotted">
             Items Purchased
           </h1>
+          <button
+            onClick={fetchPurchases}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white border rounded-md p-2 m-2">
+            Fetch Purchases
+          </button>
         </div>
         <div className="border-double border-l-4 border-slate-500"></div>
         <div className="m-5 w-1/3 flex justify-center">
           <h1 className="text-xl underline underline-offset-8 decoration-dotted">Items Sold</h1>
+          <button
+            onClick={fetchSales}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white border rounded-md p-2 m-2">
+            Fetch Sales
+          </button>
         </div>
       </div>
     </div>
