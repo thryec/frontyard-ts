284b5dec1bbc949f1a14a295d61fd566710a17ed
thryec <thryec@gmail.com>
2022-01-02 21:09:02 +0800

    changed txn post request to include entire item

--- pages/checkout/[id].tsx
+++ pages/checkout/[id].tsx
@@ -145,7 +145,7 @@ const Checkout: NextPage<itemProps> = () => {
       const initialiseTxn = {
         seller: currentItem.seller,
         buyer: walletAddress,
-        itemId: currentItem._id,
+        item: currentItem,
         salePrice: currentItem.price,
         purchaseDate: new Date(),
         orderStatus: 'Pending',
@@ -257,20 +257,20 @@ const Checkout: NextPage<itemProps> = () => {
     // add cleanup function here
   }, [])
 
-  if (userLoginState.isLoggedIn === false) {
-    return (
-      <div className="flex justify-center">
-        <div className="p-5 bg-slate-200 border rounded-md w-1/3">
-          <div className="flex justify-center mb-5">Please Log In to proceed</div>
-          <div className="flex justify-center">
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white border rounded-md p-2">
-              <Link href="/login">Go to Login </Link>
-            </button>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  // if (userLoginState.isLoggedIn === false) {
+  //   return (
+  //     <div className="flex justify-center">
+  //       <div className="p-5 bg-slate-200 border rounded-md w-1/3">
+  //         <div className="flex justify-center mb-5">Please Log In to proceed</div>
+  //         <div className="flex justify-center">
+  //           <button className="bg-indigo-600 hover:bg-indigo-700 text-white border rounded-md p-2">
+  //             <Link href="/login">Go to Login </Link>
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div>
