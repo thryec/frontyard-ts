dcdeb48c691ed45a3ed860acc498e297ac6bc582
Shin Yi <contact.shinyi@gmail.com>
2022-01-08 00:47:15 +0800

    fixed class names

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,7 +14,6 @@ const NotFound = () => {
     return (
         <>
       <main
-        aria-labelledby="pageTitle"
         className="flex items-center justify-center h-screen-10"
       >
         <div className="p-4 space-y-4">
@@ -31,9 +30,9 @@ const NotFound = () => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                   />
                 </svg>
--- pages/index.tsx
+++ pages/index.tsx
@@ -67,9 +67,9 @@ const Home: NextPage = () => {
                     </div>
 
                     <div className="flex lg:hidden">
-                        <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                        <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400">
                             <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                                <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                             </svg>
                         </button>
                     </div>
