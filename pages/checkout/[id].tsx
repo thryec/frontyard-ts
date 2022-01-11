aa41e4f5fdcadfd2fa22ea7b88fd7e3dfa771543
thryec <thryec@gmail.com>
2022-01-11 22:20:37 +0800

    resized loading gif

--- pages/checkout/[id].tsx
+++ pages/checkout/[id].tsx
@@ -607,16 +607,14 @@ const Checkout: NextPage<itemProps> = () => {
           </div>
         </div>
       ) : (
-        <div className="h-screen bg-white">
-          <div className="flex justify-center items-center h-full">
-            <Image
-              className="h-16 w-16"
-              src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif"
-              alt="loading"
-              width="200px"
-              height="200px"
-            />
-          </div>
+        <div className="flex justify-center  bg-white">
+          <Image
+            className="h-16 w-16"
+            src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif"
+            alt="loading"
+            width="100px"
+            height="100px"
+          />
         </div>
       )}
     </div>
